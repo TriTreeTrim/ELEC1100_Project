0ea550ed3bbdce4ff40ec02f06531119db8ad1d8
TriTreeTrim <95534207+TriTreeTrim@users.noreply.github.com>
2024-11-13 08:37:31 +0800

    Add files via upload

--- group_116.ino
+++ group_116.ino
@@ -5,7 +5,7 @@
 
   Group No. 116:  
   Group Member 1 Wong Hoi Ying & 21179294 :
-  Group Member 2 Mak Ho Yin & :
+  Group Member 2 Mak Ho Yin & 21063897 :
   
 */
 
@@ -14,6 +14,7 @@
 const int pinL_Sensor = A5;      //pin A5: left sensor 
 const int pinB_Sensor = A4;      //pin A4: bumper sensor
 const int pinR_Sensor = A3;      //pin A3: right sensor 
+const int pinM_Sensor = A2;      //pin A2: middle sensor
 
 const int pinL_PWM = 9;          //pin D9: left motor speed
 const int pinL_DIR = 10;         //pin D10: left motor direction
@@ -26,16 +27,22 @@ const int pinR_DIR = 12;         //pin D12: right motor direction
 int bumperSensor = 1;  // not sensing white
 int leftSensor = 1;    // not sensing white
 int rightSensor = 1;   // not sensing white
+int middleSensor = 0;  // sensing white
 
 int countBumper = 0;   // bumper sensor not triggered yet
 int countTJunc = 0;  // count no. of T-junctions encountered
 
-int startTime = 350;   // Time for car to move a bit after start line
-int turn360Time = 500; // Time for car to turn 360 degrees
+int startTime = 300;   // Time for car to move a bit after start line
+int turn360Time = 1350; // Time for car to turn 360 degrees
                       // value TO BE TESTED
-int go30cmTime = 2000;  // TASK15: Time for car to go backwards >= 30cm
+int go30cmTime = 600;  // TASK15: Time for car to go backwards >= 30cm
                         // value TO BE TESTED
 
+int inLoop = 0; // if enter loop, change pwn values
+int turn360 = 0; // faster pwn when 360 turn
+int afterCuv = 0; // change turn pwn values
+int after360 = 0; // stop before turning
+int backwards = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup ()
@@ -44,6 +51,7 @@ void setup ()
   pinMode(pinB_Sensor, INPUT);
   pinMode(pinL_Sensor, INPUT);
   pinMode(pinR_Sensor, INPUT);
+  pinMode(pinM_Sensor, INPUT);
   
   pinMode(pinL_DIR, OUTPUT);
   pinMode(pinR_DIR, OUTPUT);
@@ -61,34 +69,113 @@ void setup ()
 // define Car Turning & Moving Functions
 void turnLeft() 
 {
-  analogWrite(pinL_PWM, 200);
-  analogWrite(pinR_PWM, 200);
-  digitalWrite(pinL_DIR, 0);
-  digitalWrite(pinR_DIR, 1); 
+
+  if (after360)
+  {
+    stop();
+    delay(20);
+  }
+  if (turn360)
+  {
+    analogWrite(pinL_PWM, 400);
+    analogWrite(pinR_PWM, 400);
+    digitalWrite(pinL_DIR, 0);
+    digitalWrite(pinR_DIR, 1);
+  }
+  else if (afterCuv)
+  {
+     analogWrite(pinL_PWM, 0);
+     analogWrite(pinR_PWM, 250);
+     digitalWrite(pinL_DIR, 0);
+     digitalWrite(pinR_DIR, 1);
+  }
+  else
+  {
+     //stop();
+     //delay(5);
+     analogWrite(pinL_PWM, 105);
+     analogWrite(pinR_PWM, 140);
+     digitalWrite(pinL_DIR, 0);
+     digitalWrite(pinR_DIR, 1);
+     delay(10);
+  }
+ 
+   
 }
 
 void turnRight() 
 {
-  analogWrite(pinL_PWM, 200);
-  analogWrite(pinR_PWM, 200);
-  digitalWrite(pinL_DIR, 1);
-  digitalWrite(pinR_DIR, 0);  
+  if (after360)
+  {
+    stop();
+    delay(20);
+  }
+  if (turn360)
+  {
+    //stop();
+    //delay(5);
+    analogWrite(pinL_PWM, 400);
+    analogWrite(pinR_PWM, 400);
+    digitalWrite(pinL_DIR, 1);
+    digitalWrite(pinR_DIR, 0);
+    //delay(10);
+  }
+  else
+  {
+    //stop();
+    //delay(5);
+    analogWrite(pinL_PWM, 140);
+    analogWrite(pinR_PWM, 105);
+    digitalWrite(pinL_DIR, 1);
+    digitalWrite(pinR_DIR, 0);  
+    delay(10);
+  }
+  
 }
 
 void goForward() 
 {
-  analogWrite(pinL_PWM, 200);
-  analogWrite(pinR_PWM, 200);
-  digitalWrite(pinL_DIR, 1);
-  digitalWrite(pinR_DIR, 1);  
+  if (inLoop)
+  {
+    analogWrite(pinL_PWM, 105);
+    analogWrite(pinR_PWM, 105);
+    digitalWrite(pinL_DIR, 1);
+    digitalWrite(pinR_DIR, 1);  
+  }
+  else if (after360)
+  {
+    analogWrite(pinL_PWM, 105);
+    analogWrite(pinR_PWM, 105);
+    digitalWrite(pinL_DIR, 1);
+    digitalWrite(pinR_DIR, 1);  
+  }
+  else
+  {
+    analogWrite(pinL_PWM, 115);
+    analogWrite(pinR_PWM, 115);
+    digitalWrite(pinL_DIR, 1);
+    digitalWrite(pinR_DIR, 1);  
+  }
 }
 
 void goBackward()
 {
-  analogWrite(pinL_PWM, 200);
-  analogWrite(pinR_PWM, 200);
-  digitalWrite(pinL_DIR, 0);
-  digitalWrite(pinR_DIR, 0);
+
+  if (backwards)
+  {
+    analogWrite(pinL_PWM, 400);
+    analogWrite(pinR_PWM, 400);
+    digitalWrite(pinL_DIR, 0);
+    digitalWrite(pinR_DIR, 0);
+  }
+  else
+  {
+    analogWrite(pinL_PWM, 250);
+    analogWrite(pinR_PWM, 250);
+    digitalWrite(pinL_DIR, 0);
+    digitalWrite(pinR_DIR, 0);
+  }
+  
 }
 
 void stop()
@@ -106,6 +193,7 @@ void loop()
   bumperSensor = digitalRead(pinB_Sensor);
   leftSensor = digitalRead(pinL_Sensor);
   rightSensor = digitalRead(pinR_Sensor);
+  middleSensor = digitalRead(pinM_Sensor);
   
   // TASK1-2: car stops at the start position when bumper sensor no trigger
   if ( bumperSensor && countBumper == 0 ) 
@@ -117,7 +205,7 @@ void loop()
   else if ( !bumperSensor && countBumper == 0) 
   {
     goForward();
-    countBumper += 1;
+    countBumper++;
     delay(startTime);     //to let the car leave the start position with no miscount
   }
   
@@ -127,53 +215,106 @@ void loop()
     // when encounter T-junction
     if ( !leftSensor && !rightSensor ) 
     {
+      stop();
+      delay(100);
       switch (countTJunc)
       {
         case 0:  // TASK5
           turnLeft();
+          delay(400);
           break;
         case 1:  // TASK6
-          turnRight();
+          stop();
+          delay(30);
+          turnLeft();
+          delay(250);
+          goBackward();
+          delay(40);
           break;
         case 2:  // TASK7 - enter loop entrance
+          stop();
+          delay(30);
           turnLeft();
+          delay(350);
+          inLoop = 1;
           break;
         case 3:  // TASK7 - leave loop entrance
-          goForward();
+          stop();
+          delay(30);
+          turnLeft();
+          delay(250);
+          inLoop = 0;
           break;
         case 4:  // TASK8
           turnLeft();
+          delay(400);
           break;
         case 5:  // TASK9
           turnRight();
+          delay(250);
           break;
         case 6:  // TASK11
+          afterCuv = 1;
           turnLeft();
+          delay(200);
+          afterCuv = 0;
           break;
         case 7: // TASK12 - 360 degrees turn, then go forward a bit
+          turn360 = 1;
           turnLeft();
           delay(turn360Time);
           goForward();
+          delay(350);
+          turn360 = 0;
+          after360 = 1;
           break;
         default:
-          goForward();
+          turnRight();
+          delay(450);
       }
 
-      delay(startTime);  // a bit of delay for turning
-      countTJunc += 1;
+ 
+      countTJunc++;
     }
-    
-    if ( !leftSensor && rightSensor ) 
+    else if ( after360 && !leftSensor && !middleSensor && rightSensor)  // 001 turn 90 degree
     {
       turnLeft();
+      delay(310);
     }
-    
-    if ( leftSensor && !rightSensor ) 
+    else if ( after360 && leftSensor && !middleSensor && !rightSensor)  // 100 turn 90 degree
     {
       turnRight();
+      delay(310);
     }
     
-    if ( leftSensor && rightSensor ) 
+    else if ( !leftSensor && rightSensor ) 
+    {
+      if (after360)
+      {
+         turnLeft();
+         delay(170);
+        
+      }
+      else
+      {
+        turnLeft();
+      }
+    }
+    
+    else if ( leftSensor && !rightSensor ) 
+    {
+      if (after360)
+      {
+        turnRight();
+        delay(170);
+      }
+      else
+      {
+        turnRight();
+      }
+    }
+    
+    else if ( leftSensor && rightSensor ) 
     {
       goForward();
     }
@@ -184,9 +325,10 @@ void loop()
   else if ( !bumperSensor && countBumper == 1 )
   {
     // TASK15 - go backward for >= 30cm
+    backwards = 1;
     goBackward();
     delay(go30cmTime);
-    countBumper += 1;
+    countBumper++;
   }
 
   // TASK16 - stop after finishing TASK1-15
